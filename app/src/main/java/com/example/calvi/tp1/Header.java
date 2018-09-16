71dd9dea6fe9399f2ddf578f83b0d528d128b63a
AlexisCalvi <alex-goal73@hotmail.fr>
2018-09-16 18:15:13 +0200

    fin du design avec stylable en commentaire

--- app/src/main/java/com/example/calvi/tp1/Header.java
+++ app/src/main/java/com/example/calvi/tp1/Header.java
@@ -1,4 +1,46 @@
 package com.example.calvi.tp1;
 
-public class Header {
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Header extends LinearLayout {
+
+    public Header(Context context) {
+        super(context);
+    }
+
+    public Header(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Header(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void initialize(Context context, AttributeSet attrs) {
+        inflate(context, R.layout.header, this);
+
+        TextView mainTitleTextView = findViewById(R.id.headerTitle);
+        TextView subtitleTextView= findViewById(R.id.headerBack);
+
+        if (attrs != null) {
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.Header, 0, 0);
+
+            String title = array.getString(R.styleable.Header_mainTitle);
+            if (title != null) {
+                mainTitleTextView.setText(title);
+            }
+
+            title = array.getString(R.styleable.Header_backTitle);
+            if (title != null) {
+                subtitleTextView.setText(title);
+            }
+
+            array.recycle();
+        }
+    }
 }
--- app/src/main/java/com/example/calvi/tp1/MainActivity.java
+++ app/src/main/java/com/example/calvi/tp1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.calvi.tp1;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
