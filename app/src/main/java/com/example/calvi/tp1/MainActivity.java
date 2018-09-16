9e5799542faf1c2e1fe3fc18362dc04c927e9506
AlexisCalvi <alex-goal73@hotmail.fr>
2018-09-16 18:21:45 +0200

    fermer l'appli fonctionne

--- app/src/main/java/com/example/calvi/tp1/MainActivity.java
+++ app/src/main/java/com/example/calvi/tp1/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton close = findViewById(R.id.close);
+        TextView headerBack = findViewById(R.id.headerBack);
+        Button share = findViewById(R.id.share);
+        Button like = findViewById(R.id.like);
+        Button comment = findViewById(R.id.comment);
+
+        // Fermer l'appli
+
+        close.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MainActivity.this.finish();
+            }
+        });
+        headerBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MainActivity.this.finish();
+            }
+        });
     }
 
 
