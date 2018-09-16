ef2ad5e6c6f5a0119c57b4284c9a717612af4486
AlexisCalvi <alex-goal73@hotmail.fr>
2018-09-16 19:04:24 +0200

    final commit

--- app/src/main/java/com/example/calvi/tp1/MainActivity.java
+++ app/src/main/java/com/example/calvi/tp1/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.calvi.tp1;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -20,9 +25,10 @@ public class MainActivity extends AppCompatActivity {
         ImageButton close = findViewById(R.id.close);
         TextView headerBack = findViewById(R.id.headerBack);
         Button share = findViewById(R.id.share);
-        Button like = findViewById(R.id.like);
+        final Button like = findViewById(R.id.like);
         Button comment = findViewById(R.id.comment);
-
+        ImageButton send = findViewById(R.id.send);
+        final EditText champText = findViewById(R.id.champtext);
         // Fermer l'appli
 
         close.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +39,40 @@ public class MainActivity extends AppCompatActivity {
         headerBack.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 MainActivity.this.finish();
+            }
+        });
+
+        //like et dislike
+        like.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            public void onClick(View v) {
+
+                //cas ou on aime déja
+                if(like.getCurrentTextColor() == getResources().getColor( R.color.my_blue ) ){
+                    like.setBackground(getResources().getDrawable(R.drawable.button));
+                    like.setTextColor(getResources().getColor( R.color.my_white ));
+                    like.setCompoundDrawablesWithIntrinsicBounds( R.drawable.like, 0, 0, 0);
+                }
+                else{
+                    like.setBackground(getResources().getDrawable(R.drawable.buttonlike));
+                    like.setTextColor(getResources().getColor( R.color.my_blue ));
+                    like.setCompoundDrawablesWithIntrinsicBounds( R.drawable.likeblue, 0, 0, 0);
+                }
+            }
+        });
+
+        //Je n'ai pas compris la signification de "rester focus"..
+
+        //popup à l'envoi
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setMessage(champText.getText());
+                champText.setText("");
+                AlertDialog popUp = builder.create();
+                popUp.show();
             }
         });
     }
